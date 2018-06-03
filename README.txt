b1768f6f7eda55691396182c00496d8bb984e006
Richard Wu <wu.richard.98@gmail.com>
2018-06-02 23:47:56 -0400

    [+]Military/Civilian Mode!

---
+++ README.txt
@@ -0,0 +1,6 @@
+A digital clock whose values change the background color.
+Added option of military or civilian time.
+
+DISCLAIMER
+I did not configure this on my own!
+Reference: https://www.youtube.com/watch?v=mnAqcmVHONY
--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 var clock = document.getElementById('clock');
 var hexColor = document.getElementById('hex-color');
+var toggleMe = document.getElementById('toggle-me');
+var toggled = false;
 
 function hexClock() {
   var time = new Date();
-  var hours = time.getHours().toString();
+  var hours = (toggled && time.getHours() > 12) ? (time.getHours() % 12).toString() : time.getHours().toString();
   var minutes = time.getMinutes().toString();
   var seconds = time.getSeconds().toString();
 
@@ -27,5 +29,19 @@ function hexClock() {
 
   document.body.style.background = colorStr;
 }
-hexClock();
-setInterval(hexClock, 1000);
+
+function timeChange() {
+  if (!toggled) { // to civilian time
+    toggled = true;
+    toggleMe.textContent = "Show me Military Time!";
+    return;
+  }
+  if (toggled) { // to military time
+    toggled = false;
+    toggleMe.textContent = "Show me Civilian Time!";
+    return;
+  }
+}
+timeChange();
+hexClock(toggled);
+setInterval(hexClock, 1000, toggled);
