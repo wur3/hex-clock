d2731a47f8448f85bd4ebe3a6b60ab6e18739f36
wur3 <wu.richard.98@gmail.com>
2019-04-02 00:26:22 -0400

    Properly handles midnight

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 var clock = document.getElementById('clock');
 var hexColor = document.getElementById('hex-color');
 var toggleMe = document.getElementById('toggle-me');
-var toggled = false;
+var civilianMode = false;
 
 function hexClock() {
   var time = new Date();
-  var hours = (toggled && time.getHours() > 12) ? (time.getHours() % 12).toString() : time.getHours().toString();
+  var hours;
+  if (civilianMode && time.getHours() > 12)
+    hours = (time.getHours() % 12).toString()
+  else if (civilianMode && time.getHours() < 1)
+    hours = (time.getHours() + 12).toString()
+  else
+    hours = time.getHours().toString();
   var minutes = time.getMinutes().toString();
   var seconds = time.getSeconds().toString();
 
@@ -31,17 +37,17 @@ function hexClock() {
 }
 
 function timeChange() {
-  if (!toggled) { // to civilian time
-    toggled = true;
+  if (!civilianMode) { // to civilian time
+    civilianMode = true;
     toggleMe.textContent = "Show me Military Time!";
     return;
   }
-  if (toggled) { // to military time
-    toggled = false;
+  if (civilianMode) { // to military time
+    civilianMode = false;
     toggleMe.textContent = "Show me Civilian Time!";
     return;
   }
 }
 timeChange();
-hexClock(toggled);
-setInterval(hexClock, 1000, toggled);
+hexClock(civilianMode);
+setInterval(hexClock, 1000, civilianMode);
